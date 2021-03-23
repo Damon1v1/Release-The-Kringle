3d99ffe11aebb367255bace6a38dc18f3c1ccb56
kjwallac <kristinajwallace@gmail.com>
2021-03-22 22:28:09 -0700

    db & seed sql files, added menu-routes and product model

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 // const userRoutes = require('./userRoutes');
-const menuRoutes = require('./menuRoutes');
+const menuRoutes = require('./menu-routes');
 
 // router.use('/users', userRoutes);
 router.use('/menu', menuRoutes);
