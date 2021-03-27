3bdbc9e72b8cb58bec8aa36e1bec92029269ae50
Damon Burda <damonburda96@gmail.com>
2021-03-27 10:29:32 -0700

    Signup handlebars and updated user model

--- models/User.js
+++ models/User.js
@@ -6,7 +6,15 @@ User.init(
         primaryKey: true,
         autoIncrement: true
       },
-      username: {
+      first_name: {
+        type: DataTypes.STRING,
+        allowNull: false
+      },
+      last_name: {
+        type: DataTypes.STRING,
+        allowNull: false
+      },
+      email: {
         type: DataTypes.STRING,
         allowNull: false
       },
