d539cb1d6d135d6bf9c3206aacdcd42dcbdd5c94
kjwallac <kristinajwallace@gmail.com>
2021-03-27 12:39:11 -0700

    rendered images for each product. added use of serve-static to server.js

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const express = require('express');
 const exphbs = require('express-handlebars');
+const serveStatic = require('serve-static');
 const menuRoutes = require('./controllers/menu-routes');
 
 const sequelize = require('./config/connection');
@@ -11,18 +12,17 @@ const PORT = process.env.PORT || 3002;
 
 const hbs = exphbs.create();
 
+
+
 // Inform Express.js on which template engine to use
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, 'public')));
-
-
 
 app.use(menuRoutes);
-
+app.use(serveStatic(path.join(__dirname, 'public')));
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}!`);
   sequelize.sync({ force: false });
