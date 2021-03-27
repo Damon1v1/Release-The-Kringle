369ae6ca1cdf745631fea628c8c5f30ec9cb54d2
Damon1v1 <71532303+Damon1v1@users.noreply.github.com>
2021-03-27 14:42:16 -0700

    Update User.js

--- models/User.js
+++ models/User.js
@@ -1,3 +1,13 @@
+const { Model, DataTypes } = require('sequelize');
+const bcrypt = require('bcrypt');
+const sequelize = require('../config/connection');
+
+class User extends Model {
+  checkPassword(loginPw) {
+    return bcrypt.compareSync(loginPw, this.password);
+  }
+}
+
 User.init(
     {
       id: {
@@ -52,3 +62,5 @@ User.init(
       modelName: 'User'
     }
   );
+
+module.exports = new User;
