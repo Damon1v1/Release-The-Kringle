ddda18ca88463c7955791615978c69c0db60bfb5
Damon Burda <damonburda96@gmail.com>
2021-03-28 14:24:15 -0700

    Create Session and cookie

--- server.js
+++ server.js
@@ -2,18 +2,29 @@ const path = require('path');
 const express = require('express');
 const exphbs = require('express-handlebars');
 const serveStatic = require('serve-static');
+const session = require('express-session');
 const menuRoutes = require('./controllers/menu-routes');
 const homeRoutes = require('./controllers/home-routes');
 
 const sequelize = require('./config/connection');
-
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const app = express();
 const PORT = process.env.PORT || 3002;
 
 const hbs = exphbs.create();
 
+const sess = {
+  secret: 'Super secret secret',
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize
+  })
+};
 
+app.use(session(sess));
 
 // Inform Express.js on which template engine to use
 app.engine('handlebars', hbs.engine);
