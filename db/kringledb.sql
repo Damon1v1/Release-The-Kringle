baa613639fc9b4f0afbb757845abd1df51cfda04
Damon Burda <damonburda96@gmail.com>
2021-03-27 21:39:53 -0700

    fixed user.js

--- db/kringledb.sql
+++ db/kringledb.sql
@@ -17,10 +17,11 @@ CREATE TABLE product(
 
 CREATE TABLE user(
     id INT NOT NULL AUTO_INCREMENT,
-    username VARCHAR(50) NOT NULL,
-    password VARCHAR(50) NOT NULL,
     first_name VARCHAR(50) NOT NULL,
     last_name VARCHAR(50) NOT NULL,
+    email VARCHAR(50) NOT NULL,
+    password VARCHAR(50) NOT NULL,
+    points INT DEFAULT (500),
     PRIMARY KEY (id)
 );
 
