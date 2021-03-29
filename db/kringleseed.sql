9e50cde6fec6e1e524a454d14ef08a42009c7eed
kjwallac <kristinajwallace@gmail.com>
2021-03-28 20:53:13 -0700

    update db seed images

--- db/kringleseed.sql
+++ db/kringleseed.sql
@@ -6,10 +6,10 @@ VALUES
 ('Kringle - Raspberry','Our classic kringle with raspberry filling',15.00,'raspberrykringle.jpg',FALSE,TRUE),
 ('Kringle - Apple','Our classic kringle with apple filling',15.00,'applekringle.jpg',FALSE,TRUE),
 ('Kringle - Pecan','Our classic kringle with pecan filling',15.00,'pecankringle.jpg',TRUE,TRUE),
-('Kringle - Cherry Cheese','Our classic kringle with cherry cheese filling',15.00,'cherrycheese.jpg',FALSE,TRUE),
+('Kringle - Cherry Cheese','Our classic kringle with cherry cheese filling',15.00,'cherrycheesekringle.jpg',FALSE,TRUE),
 ('Kringle - Apricot','Our classic kringle with apricot filling',15.00,'apricotkringle.jpg',FALSE,TRUE),
-('Kringle - Cream Cheesecake','Our classic kringle with cream cheesecake filling',15.00,'creamcheesecase.jpg',FALSE,TRUE),
-('Mini Kringle - Raspberry','A mini version of our classic kringle with raspberry filling',7.00,'miniraspberrykringle.jpg',FALSE,TRUE),
-('Mini Kringle - Apple','A mini version of our classic kringle with apple filling',7.00,'miniapplekringle.jpg',FALSE,TRUE),
-('Mini Kringle - Pecan','A mini version of our classic kringle with pecan filling',7.00,'minipecankringle.jpg',TRUE,TRUE),
-('Gluten Free Kringle - Apple','A gluten free version of our classic kringle with apple filling',25.00,'glutenfreeapplekringle.jpg',FALSE,FALSE);
+('Kringle - Cream Cheesecake','Our classic kringle with cream cheesecake filling',15.00,'creamcheesekringle.jpg',FALSE,TRUE),
+('Mini Kringle - Raspberry','A mini version of our classic kringle with raspberry filling',7.00,'raspberrykringle.jpg',FALSE,TRUE),
+('Mini Kringle - Apple','A mini version of our classic kringle with apple filling',7.00,'applekringle.jpg',FALSE,TRUE),
+('Mini Kringle - Pecan','A mini version of our classic kringle with pecan filling',7.00,'pecankringle.jpg',TRUE,TRUE),
+('Gluten Free Kringle - Apple','A gluten free version of our classic kringle with apple filling',25.00,'applekringle.jpg',FALSE,FALSE);
